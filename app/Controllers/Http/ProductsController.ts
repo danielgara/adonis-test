b51771912d20030574656c04dd1ab7a65ef5a445
Daniel Correa <yo@danielgara.com>
2023-02-08 15:08:40 -0500

    imp: get products from database

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -1,50 +1,22 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Product from 'App/Models/Product';
 
 export default class ProductsController {
-  static products = [
-    {
-      id: '1',
-      name: 'TV',
-      description: 'Best tv',
-      image: 'game.png',
-      price: '1000',
-    },
-    {
-      id: '2',
-      name: 'iPhone',
-      description: 'Best iPhone',
-      image: 'safe.png',
-      price: '999',
-    },
-    {
-      id: '3',
-      name: 'Chromecast',
-      description: 'Best Chromecast',
-      image: 'submarine.png',
-      price: '30',
-    },
-    {
-      id: '4',
-      name: 'Glasses',
-      description: 'Best Glasses',
-      image: 'game.png',
-      price: '100',
-    },
-  ];
-
   public async index(ctx: HttpContextContract) {
     const viewData = [];
     viewData['title'] = 'Products - Online Store';
     viewData['subtitle'] = 'List of products';
-    viewData['products'] = ProductsController.products;
+    viewData['products'] = await Product.all();
     return ctx.view.render('products/index', { viewData: viewData });
   }
 
   public async show(ctx: HttpContextContract) {
-    const product = ProductsController.products[ctx.params.id - 1];
+    const product = await Product.find(ctx.params.id);
     const viewData = [];
-    viewData['title'] = product.name + ' - Online Store';
-    viewData['subtitle'] = product.name + ' - Product Information';
+    if (product) {
+      viewData['title'] = product.name + ' - Online Store';
+      viewData['subtitle'] = product.name + ' - Product Information';
+    }
     viewData['product'] = product;
     return ctx.view.render('products/show', { viewData: viewData });
   }
