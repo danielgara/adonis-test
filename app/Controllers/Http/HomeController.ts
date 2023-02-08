c1ffb5ac6c6507b58c72a792af92cf2c04dbccf1
Daniel Correa <yo@danielgara.com>
2023-02-08 11:41:19 -0500

    imp: about section

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -2,6 +2,17 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class PostsController {
   public async index(ctx: HttpContextContract) {
-    return ctx.view.render('home');
+    const viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return ctx.view.render('home', { viewData: viewData });
+  }
+
+  public async about(ctx: HttpContextContract) {
+    const viewData = [];
+    viewData['title'] = 'About us - Online Store';
+    viewData['subtitle'] = 'About us';
+    viewData['description'] = 'This is an about page ...';
+    viewData['author'] = 'Developed by: Your Name';
+    return ctx.view.render('about', { viewData: viewData });
   }
 }
