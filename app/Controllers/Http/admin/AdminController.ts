6ac060f3c68e317dd3565df344390db1022031ca
Daniel Correa <yo@danielgara.com>
2023-02-09 18:13:00 -0500

    fix: admin controller

--- app/Controllers/Http/admin/AdminController.ts
+++ app/Controllers/Http/admin/AdminController.ts
@@ -2,9 +2,6 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class AdminController {
   public async index(ctx: HttpContextContract) {
-    if (ctx.auth.user) {
-      console.log(ctx.auth.user.getRole());
-    }
     const viewData = [];
     viewData['title'] = 'Admin Page - Admin - Online Store';
     return ctx.view.render('admin/index', { viewData: viewData });
