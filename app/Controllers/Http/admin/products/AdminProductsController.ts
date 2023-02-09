3a4f6757f3591d613fcb1990821c45dab2644d62
Daniel Correa <yo@danielgara.com>
2023-02-09 10:40:47 -0500

    imp: upload images

--- app/Controllers/Http/admin/products/AdminProductsController.ts
+++ app/Controllers/Http/admin/products/AdminProductsController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Product from 'App/Models/Product';
+import Application from '@ioc:Adonis/Core/Application';
 
 export default class AdminProductsController {
   public async index(ctx: HttpContextContract) {
@@ -10,11 +11,19 @@ export default class AdminProductsController {
   }
 
   public async store(ctx: HttpContextContract) {
+    const productImage = ctx.request.file('image');
+    let fileName = '';
+    if (productImage) {
+      await productImage.move(Application.publicPath('uploads'));
+      if (productImage.fileName != undefined) {
+        fileName = productImage.fileName;
+      }
+    }
     const newProduct = new Product();
     newProduct.setName(ctx.request.input('name'));
     newProduct.setDescription(ctx.request.input('description'));
     newProduct.setPrice(ctx.request.input('price'));
-    newProduct.setImage('game.png');
+    newProduct.setImage(fileName);
     newProduct.save();
     ctx.response.redirect().toPath('/admin/products');
   }
