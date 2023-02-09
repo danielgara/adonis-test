67d845d07b08831efdeb081855d9a52298055691
Daniel Correa <yo@danielgara.com>
2023-02-09 10:21:13 -0500

    imp: add product

--- app/Controllers/Http/admin/products/AdminProductsController.ts
+++ app/Controllers/Http/admin/products/AdminProductsController.ts
@@ -8,4 +8,14 @@ export default class AdminProductsController {
     viewData['products'] = await Product.all();
     return ctx.view.render('admin/products/index', { viewData: viewData });
   }
+
+  public async store(ctx: HttpContextContract) {
+    const newProduct = new Product();
+    newProduct.setName(ctx.request.input('name'));
+    newProduct.setDescription(ctx.request.input('description'));
+    newProduct.setPrice(ctx.request.input('price'));
+    newProduct.setImage('game.png');
+    newProduct.save();
+    ctx.response.redirect().toPath('/admin/products');
+  }
 }
--- start/routes/admin.ts
+++ start/routes/admin.ts
@@ -2,3 +2,4 @@ import Route from '@ioc:Adonis/Core/Route';
 
 Route.get('/admin', 'admin/AdminController.index');
 Route.get('/admin/products', 'admin/products/AdminProductsController.index');
+Route.post('/admin/products/store', 'admin/products/AdminProductsController.store');
